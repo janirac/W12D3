fa76c90336b4d1622d9d69c0125e174364aa693e
Janira Crispin <janiracrispin@gmail.com>
2023-02-02 09:58:36 -0500

    complete backend phase 3

--- authenticate-me/app/controllers/api/sessions_controller.rb
+++ authenticate-me/app/controllers/api/sessions_controller.rb
@@ -17,7 +17,7 @@ class Api::SessionsController < ApplicationController
 
       @user = User.find_by_credentials(credentials, password)
 
-      if @user 
+      if @user.save
          login!(@user)
          render 'api/users/show'
       else 
